05b9091c233539be35479433b342c31a44c7b39b
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-09-22 17:02:17 +0100

    decimal rounding up

--- admin/test.php
+++ admin/test.php
@@ -4,5 +4,11 @@ include("functions/init.php");
 //echo "https://student.dagloremodelschool.com.ng/qrnt";
 
 echo $call['stud']."/qrnt";
+
+echo(round(4.96754,2) . "<br>");
+echo(round(7.045,2) . "<br>");
+echo(round(7.055,2));
+
+
   
 //echo strtotime($);
